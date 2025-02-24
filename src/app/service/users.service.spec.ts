0021af8a0bb6b314490c0b4175f678c4a436c9cb
ndt2-0-1-2-4 <nguyentri1o2.z@gmail.com>
2025-02-24 22:13:08 +0700

    layout

--- src/app/service/users.service.spec.ts
+++ src/app/service/users.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UsersService } from './users.service';
+import { userService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let service: userService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UsersService);
+    service = TestBed.inject(userService);
   });
 
   it('should be created', () => {
