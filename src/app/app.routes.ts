86b9938ce0e14a60a4aa80f377b375b2b652a120
ndt2-0-1-2-4 <nguyentri1o2.z@gmail.com>
2025-02-27 00:04:04 +0700

    friend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { FriendComponent } from './friend/friend.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    {path: 'user', component: UserComponent}
+    {path: 'user', component: UserComponent},
+    {path: 'friend', component: FriendComponent}
 ];
