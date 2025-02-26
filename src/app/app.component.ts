78badfe205ec911ef362b93da33a34102b5f8057
ndt2-0-1-2-4 <nguyentri1o2.z@gmail.com>
2025-02-26 09:46:29 +0700

    user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,9 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { LeftsideComponent } from "./shared/leftside/leftside.component";
 import { RightsideComponent } from "./shared/rightside/rightside.component";
-import { UserComponent } from "./user/user.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LeftsideComponent, RightsideComponent, LoginComponent, NgIf, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, LeftsideComponent, RightsideComponent, LoginComponent, NgIf, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
