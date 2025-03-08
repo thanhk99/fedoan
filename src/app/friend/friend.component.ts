45b8c811593e2c1799066e1111e1835cdc2eb106
Chithanh1709 <chipngaocho123@gmail.com>
2025-03-08 22:07:03 +0700

    chithanh2

--- src/app/friend/friend.component.ts
+++ src/app/friend/friend.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-friend',
-  imports: [CommonModule],
+  imports: [CommonModule, BrowserModule, FormsModule],
   templateUrl: './friend.component.html',
   styleUrl: './friend.component.css'
 })
@@ -11,6 +14,12 @@ export class FriendComponent {
 [x: string]: any;
 showFriendsList = false; // Biến kiểm soát hiển thị danh sách bạn bè
 showFriendRequest = false; // Biến kiểm soát hiển thị danh sách lời mời kết bạn
+searchQuery = ''; // Biến lưu trữ từ khóa tìm kiếm
+showSearchResults = false; // Biến kiểm soát hiển thị kết quả tìm kiếm người kết bạn
+searchResults: string[] = []; // Danh sách kết quả tìm kiếm người kết bạn
+
+
+
 
 friends = ['Nguyễn Chí Thành', 'Nguyễn Đức Trí', 'Phạm Như Thành', 'Phạm Trường Giang']; // Danh sách bạn bè
 friendRequests = [{ name: 'Lê Hồng Phong' }, { name: 'Bùi Thị Phương Mỹ' }]; // Danh sách lời mời kết bạn
@@ -25,4 +34,13 @@ showFriendRequests() {
   this.showFriendRequest = true; // Hiển thị danh sách lời mời kết bạn
 }
   
+searchFriends() {
+  this.showFriendsList = false;
+  this.showFriendRequest = false;
+  this.showSearchResults = true;
+
+  const query = this.searchQuery.toLowerCase();
+  this.searchResults = this.friends.filter(friend => friend.toLowerCase().includes(query));
+}
+
 }
