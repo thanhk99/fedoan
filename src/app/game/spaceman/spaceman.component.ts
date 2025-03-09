7f78ffdfe9ec0d0246930976ee8138e656dfbf37
Chithanh1709 <chipngaocho123@gmail.com>
2025-03-09 15:37:41 +0700

    Friend Final

---
+++ src/app/game/spaceman/spaceman.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spaceman',
+  imports: [],
+  templateUrl: './spaceman.component.html',
+  styleUrl: './spaceman.component.css'
+})
+export class SpacemanComponent {
+
+}
