9f2bc8a9bea17e7e683163b960eacea7ebd396e4
Chithanh1709 <chipngaocho123@gmail.com>
2025-03-26 20:26:54 +0700

    ct

---
+++ src/app/service/friend.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { endWith, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service'
+import * as CryptoJS from 'crypto-js';
+import { environment } from '../../environments/environment';
+import { format } from 'date-fns';
+import { userService } from './users.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendService {
+
+  constructor(private http: HttpClient,private cookieService: CookieService , 
+    private userService : userService){}
+  private apigetFriend = environment.apigetFriend;
+  private apiaddFriend = environment.apiaddFriend;
+  private apideleFriend = environment.apideleFriend;
+  private apiacceptFriend = environment.apiacceptFriend;
+  private apigetFriendRequets = environment.apiGetrequets;
+  private apideleFriendRequets = environment.apideleFriendRequets;
+
+  getFriends(): Observable<any> {
+    const idMy = this.userService.getCookies();
+    return this.http.post(this.apigetFriend, {idMy});
+  }
+
+  getIdFriend(): void {
+    this.userService.getUser().subscribe(user => {
+        const idFriend = user.id; // Lấy ID từ getUser()
+        console.log("ID của bạn bè:", idFriend);
+    }, error => {
+        console.error("Lỗi khi lấy ID:", error);
+    });
+}
+  
+  getFriendRequets():Observable<any>{
+    const idMy = this.userService.getCookies();
+    return this.http.post(this.apigetFriendRequets,{idMy});
+  }
+
+  // Gửi lời mời kết bạn
+  addFriend(): Observable<any> {
+    const idMy = this.userService.getCookies();
+    const idFriend = this.getIdFriend();
+    return this.http.post<any>(this.apiaddFriend, { idMy , idFriend });
+  }
+
+  // Xóa bạn bè
+  deleteFriend(): Observable<any> {
+    const idMy = this.userService.getCookies();
+    const idFriend = this.getIdFriend();
+    return this.http.delete<any>(`${this.apideleFriend}/${idMy}/${idFriend}`);
+  }
+
+  //Xóa lời mời kb
+
+  deleteFriendRequets(): Observable<any>{
+    const idFriend = this.getIdFriend();
+    const idMy = this.userService.getCookies();
+    return this.http.delete<any>(`${this.apideleFriend}/${idMy}/${idFriend}`);
+
+  }
+
+
+
+  // Chấp nhận lời mời kết bạn
+  acceptFriend(): Observable<any> {
+    const idMy = this.userService.getCookies();
+    const idFriend = this.getIdFriend();
+    return this.http.post<any>(this.apiacceptFriend, {idMy , idFriend});
+  }
+  
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-const SERVER_IP = '192.168.0.101'; 
+const SERVER_IP = '192.168.1.83'; 
 
 export const environment = {
     production: true,
@@ -10,5 +10,12 @@ export const environment = {
     apiSaveHisPlayer: `http://${SERVER_IP}:8082/game/savePlayerHis`,
     apiSaveHisBalan:`http://${SERVER_IP}:8082/Atm/saveHis`,
     apiupdateBalan:`http://${SERVER_IP}:8082/Atm/updateBalan`,
+    apigetFriend :`http://${SERVER_IP}:8082/friend/getListFriend`,
+    apiaddFriend :`http://${SERVER_IP}:8082/friend/addFriend`,
+    apiacceptFriend :`http://${SERVER_IP}:8082/friend/acceptFriend`,
+    apideleFriend :`http://${SERVER_IP}:8082/friend/deleteFriend`,
+    apideleFriendRequets :`http://${SERVER_IP}:8082/friend/deleteFriendRequest`,
+    apiSearch :`http://${SERVER_IP}:8082/user/searchFullname`,
+    apiGetrequets:`http://${SERVER_IP}:8082/friend/getFriendRequests`,
     keysecret:"anhthanhdz",
 };
