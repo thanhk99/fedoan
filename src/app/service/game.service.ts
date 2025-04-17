a84a391ed1ff633cbc5b30e164251827d03dccfa
thanhk99 <thanh040804@gmail.com>
2025-04-17 20:41:12 +0700

    fix api Football v5

--- src/app/service/game.service.ts
+++ src/app/service/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,7 +16,12 @@ export class GameService {
     const body = {"namegame":namegame}
     return this.http.post(environment.apiGetHistoryGame,body)
   }
-  getMatches(dateFrom: string, dateTo: string){
-    return this.http.get(`${this.apiFootBall}?dateFrom=${dateFrom}&dateTo=${dateTo}`);
+  getMatches(dateFrom: string, dateTo: string): Observable<any> {
+    // Thêm header ngrok-skip-browser-warning
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json')
+      .set('ngrok-skip-browser-warning', 'true'); // Giá trị bất kỳ, ví dụ 'true'
+
+    return this.http.get(`${this.apiFootBall}?dateFrom=${dateFrom}&dateTo=${dateTo}`, { headers });
   }
 }
